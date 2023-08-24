0b63424acc58bfbcf20b23632c5f8364595b9fd6
Suleymanmert <mertsuleyman0101@gmail.com>
2023-08-24 23:02:03 +0300

    24,08,2023

---
+++ src/day43_collections/C04_HashSet.java
@@ -0,0 +1,15 @@
+package day43_collections;
+
+import java.util.HashSet;
+
+public class C04_HashSet {
+    public static void main(String[] args) {
+        //Soru : Bir HashSet ve TreeSet olusturun.
+        // Bir loop ile bu set’lere element ekleyip sureleri karsilastirin.
+
+
+
+
+    }
+
+}
